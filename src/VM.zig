e2f6e156a144beb8be6129140b2ea6d42c00ddf5
Yuhao Chen <hello@chenyuhao.me>
2026-05-13 00:37:01 -0700

    a value stack manipulator

--- src/VM.zig
+++ src/VM.zig
@@ -12,33 +12,63 @@ const VM = @This();
 
 const InterpretResult = enum(u8) { interpret_ok, interpret_compile_error, interpret_runtime_error };
 
+const stack_max: usize = 256;
+
 // Chunk
 
 chunk: *Chunk,
 ip: [*]u8,
+stack: [stack_max]Value,
+stack_top: [*]Value,
+
+fn resetStack(vm: *VM) void {
+    vm.stack_top = &vm.stack;
+}
 
 pub fn init(vm: *VM) void {
-    _ = vm;
+    vm.resetStack();
 }
 
 pub fn free(vm: *VM) void {
     _ = vm;
 }
 
+pub fn push(vm: *VM, value: Value) void {
+    vm.stack_top[0] = value;
+    vm.stack_top += 1;
+}
+
+pub fn pop(vm: *VM) Value {
+    vm.stack_top -= 1;
+    return vm.stack_top[0];
+}
+
 pub fn run(vm: *VM) InterpretResult {
     while (true) {
+        if (common.debug_trace_execution) {
+            print("          ", .{});
+            var slot: [*]Value = &vm.stack;
+            while (slot != vm.stack_top) : (slot += 1) {
+                print("[ ", .{});
+                val.printValue(slot[0]);
+                print(" ]", .{});
+            }
+            print("\n", .{});
+            _ = debug.disassembleInstruction(vm.chunk, @intFromPtr(vm.ip) - @intFromPtr(vm.chunk.code.?));
+        }
+
         const instruction = vm.readBytes();
         const op: Chunk.OpCode = @enumFromInt(instruction);
         switch (op) {
             .op_constant => {
                 const constant: Value = readConstant(vm);
-                val.printValue(constant);
-                print("\n", .{});
-                if (common.debug_trace_execution) {
-                    _ = debug.disassembleInstruction(vm.chunk, vm.ip - vm.chunk.code.?);
-                }
+                vm.push(constant);
             },
-            .op_return => return .interpret_ok,
+            .op_return => {
+                val.printValue(vm.pop());
+                print("\n", .{});
+                return .interpret_ok;
+            },
         }
     }
 }
