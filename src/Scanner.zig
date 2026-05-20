74caa3dd22272891866432a62967b3d30ee9c59b
Yuhao Chen <hello@chenyuhao.me>
2026-05-19 20:21:29 -0700

    one token at a time

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -1,11 +1,69 @@
+pub const TokenType = enum {
+    // single character tokens
+    left_paren,
+    right_paren,
+    left_brace,
+    right_brace,
+    comma,
+    dot,
+    minus,
+    plus,
+    semicolon,
+    slash,
+    star,
+    // one or two character tokens
+    bang,
+    bang_equal,
+    equal,
+    equal_equal,
+    greater,
+    greater_equal,
+    less,
+    less_equal,
+    // literals
+    identifier,
+    string,
+    number,
+    // keywords
+    @"and",
+    class,
+    @"else",
+    false,
+    @"for",
+    fun,
+    @"if",
+    nil,
+    @"or",
+    print,
+    @"return",
+    super,
+    this,
+    true,
+    @"var",
+    @"while",
+    @"error",
+    eof,
+};
+
+pub const Token = struct { type: TokenType, start: [*]const u8, length: usize, line: i32 };
+
 const Scanner = @This();
 
 start: []const u8,
 current: []const u8,
-line: u32,
+line: i32,
 
 pub fn init(scanner: *Scanner, source: []const u8) void {
     scanner.start = source;
     scanner.current = source;
     scanner.line = 1;
+}
+
+pub fn scanToken(scanner: *Scanner) Token {
+    return .{
+        .type = .eof,
+        .start = scanner.current.ptr,
+        .length = 0,
+        .line = scanner.line,
+    };
 }
--- src/compiler.zig
+++ src/compiler.zig
@@ -1,6 +1,27 @@
+const std = @import("std");
+
+const print = std.debug.print;
+
 const Scanner = @import("Scanner.zig");
 
 pub fn compile(source: []const u8) void {
     var scanner: Scanner = undefined;
     scanner.init(source);
+
+    var line: i32 = -1;
+
+    while (true) {
+        const token = scanner.scanToken();
+
+        if (token.line != line) {
+            print("{d:<4} ", .{token.line});
+            line = token.line;
+        } else {
+            print("    | ", .{});
+        }
+
+        print("{d:<2} '{s}'\n", .{ @intFromEnum(token.type), token.start[0..token.length] });
+
+        if (token.type == .eof) break;
+    }
 }
