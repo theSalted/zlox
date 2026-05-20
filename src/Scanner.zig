9a887b135d8cc384f04553f9103c2996b5626d04
Yuhao Chen <hello@chenyuhao.me>
2026-05-19 19:39:40 -0700

    minor issues

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -4,7 +4,7 @@ start: []const u8,
 current: []const u8,
 line: u32,
 
-pub fn initScanner(scanner: *Scanner, source: []const u8) void {
+pub fn init(scanner: *Scanner, source: []const u8) void {
     scanner.start = source;
     scanner.current = source;
     scanner.line = 1;
--- src/compiler.zig
+++ src/compiler.zig
@@ -1,6 +1,6 @@
-const Scanner = @import("scanner.zig");
+const Scanner = @import("Scanner.zig");
 
 pub fn compile(source: []const u8) void {
     var scanner: Scanner = undefined;
-    scanner.initScanner(source);
+    scanner.init(source);
 }
