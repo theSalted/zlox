ba201728f8d2cad899e37304c7a0b5cbb52772a6
Yuhao Chen <hello@chenyuhao.me>
2026-05-21 23:22:59 -0700

    compiler and parser scafolding

--- src/compiler.zig
+++ src/compiler.zig
@@ -9,7 +9,6 @@ const TokenType = Scanner.TokenType;
 
 const Parser = struct {
     scanner: *Scanner,
-    compilingChunk: *Chunk,
     previous: Token,
     current: Token,
     hadError: bool,
@@ -30,12 +29,6 @@ const Parser = struct {
         }
     }
 
-    fn expression(parser: *Parser) void {
-        // COMEBACK
-
-        _ = parser;
-    }
-
     fn consume(parser: *Parser, token_type: TokenType, message: []const u8) void {
         if (parser.current.type == token_type) {
             parser.advance();
@@ -43,14 +36,6 @@ const Parser = struct {
         }
 
         parser.errorAtCurrent(message);
-    }
-
-    fn emitByte(parser: *Parser, byte: u8) void {
-        parser.currentChunk().write(byte, parser.previous.line);
-    }
-
-    fn currentChunk(parser: *Parser) *Chunk {
-        return parser.compilingChunk;
     }
 
     fn errorAtCurrent(parser: *Parser, message: []const u8) void {
@@ -79,19 +64,78 @@ const Parser = struct {
     }
 };
 
+const Compiler = struct {
+    parser: *Parser,
+    compilingChunk: *Chunk,
+
+    fn init(c: *Compiler, parser: *Parser, chunk: *Chunk) void {
+        c.parser = parser;
+        c.compilingChunk = chunk;
+    }
+
+    fn expression(compiler: *Compiler) void {
+        // COMEBACK
+
+        _ = compiler;
+    }
+
+    fn emitByte(compiler: *Compiler, byte: u8) void {
+        compiler.currentChunk().write(byte, compiler.parser.previous.line);
+    }
+
+    fn emitBytes(compiler: *Compiler, byte1: u8, byte2: u8) void {
+        compiler.emitByte(byte1);
+        compiler.emitByte(byte2);
+    }
+
+    fn emitReturn(compiler: *Compiler) void {
+        compiler.emitByte(@intFromEnum(Chunk.OpCode.op_return));
+    }
+
+    fn makeConstant(compiler: *Compiler, value: f64) u8 {
+        const constant = compiler.currentChunk().addConstant(value);
+        if (constant > 255) {
+            compiler.parser.errorAtPrevious("Too many constants in one chunk");
+            return 0;
+        }
+        return @intCast(constant);
+    }
+
+    fn emitConstant(compiler: *Compiler, value: f64) void {
+        compiler.emitBytes(@intFromEnum(Chunk.OpCode.op_constant), compiler.makeConstant(value));
+    }
+
+    fn currentChunk(compiler: *Compiler) *Chunk {
+        return compiler.compilingChunk;
+    }
+
+    fn endCompiler(compiler: *Compiler) void {
+        compiler.emitReturn();
+    }
+
+    fn number(compiler: *Compiler) void {
+        const lexeme = compiler.parser.previous.start[0..compiler.parser.previous.length];
+        const value = std.fmt.parseFloat(f64, lexeme) catch unreachable;
+        compiler.emitConstant(value);
+    }
+};
+
 pub fn compile(source: []const u8, chunk: *Chunk) bool {
     var scanner: Scanner = undefined;
     scanner.init(source);
 
     var parser: Parser = undefined;
     parser.init(&scanner);
-    parser.compilingChunk = chunk;
     parser.hadError = false;
     parser.panicMode = false;
 
+    var compiler: Compiler = undefined;
+    compiler.init(&parser, chunk);
+
     parser.advance();
-    parser.expression();
+    compiler.expression();
     parser.consume(.eof, "Expect end of expression.");
+    compiler.endCompiler();
 
     return !parser.hadError;
 }
